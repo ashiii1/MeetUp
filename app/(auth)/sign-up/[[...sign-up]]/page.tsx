191e1c2e920f886f45e23730709d0e768a83be95
ashiii1 <aashishaik526@gmail.com>
2024-07-09 19:46:23 +0530

    see

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,27 +1,14 @@
+// Import SignUp from Clerk.js
 import { SignUp } from '@clerk/nextjs';
-import { GetStaticPaths, GetStaticProps } from 'next';
 
-// Define the paths you want to pre-render
-export async function generateStaticParams() {
-  const paths = [
-    { params: { 'sign-up': [] } }, // pre-render /sign-up
-    // Add more paths if needed
-  ];
-
-  return paths;
-}
-
-// Fetch data based on context.params['sign-up']
-export const getStaticProps: GetStaticProps = async (context) => {
-  return {
-    props: {}, // pass data to the page component as props if needed
-  };
-};
-
-export default function SignUpPage() {
+// Define your SignUpPage component
+const SignUpPage = () => {
   return (
     <main className="flex h-screen w-full items-center justify-center">
       <SignUp />
     </main>
   );
-}
+};
+
+// Export your SignUpPage component as default
+export default SignUpPage;
