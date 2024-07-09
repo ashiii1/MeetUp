c167b7152d5ea6aa9142b73c1a8196e632cbf0b9
ashiii1 <aashishaik526@gmail.com>
2024-07-09 19:24:16 +0530

    d

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,26 @@
 import { SignUp } from '@clerk/nextjs';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+// Define the paths you want to pre-render
+export async function generateStaticParams() {
+  const paths = [
+    { params: { 'sign-up': [] } }, // pre-render /sign-up
+    // Add more paths if needed
+  ];
+
+  return paths;
+}
+
+// Fetch data based on context.params['sign-up']
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {}, // pass data to the page component as props if needed
+  };
+};
 
 export default function SignUpPage() {
   return (
-    <main className=" flex h-screen w-full items-center justify-center">
+    <main className="flex h-screen w-full items-center justify-center">
       <SignUp />
     </main>
   );
